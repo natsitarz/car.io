d166d8eb64f66b17587489f820033d423f3939ef
natsitarz <natsitarz@icloud.com>
2024-02-06 16:36:57 +0100

    first commit

---
+++ scripts/car-page.js
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////
+window.onload = function() {
+    let carManuModelText = document.getElementById("car-manumodel");
+    let carYearText = document.getElementById("car-year");
+    let carEngineText = document.getElementById("car-engine");
+    let carFuelText = document.getElementById("car-fuel");
+    let carTransmissionText = document.getElementById("car-transm");
+    let carDescText = document.getElementById("car-desc");
+    let carOwner = document.getElementById("car-owner");
+    carManuModelText.innerHTML = `${JSON.parse(localStorage.getItem("firstCar")).manufacturer} ${JSON.parse(localStorage.getItem("firstCar")).model}`;
+    carYearText.innerHTML = JSON.parse(localStorage.getItem("firstCar")).year;
+    carEngineText.innerHTML = JSON.parse(localStorage.getItem("firstCar")).engine;
+    carFuelText.innerHTML = JSON.parse(localStorage.getItem("firstCar")).fuel;
+    carTransmissionText.innerHTML = JSON.parse(localStorage.getItem("firstCar")).transmission;
+    carOwner.innerHTML = `Owner: ${JSON.parse(localStorage.getItem("firstCar")).owner}`;
+    if (localStorage.getItem("carDescription") == null) {
+        localStorage.setItem("carDescription", "No description yet");
+    } else {
+        carDescText.innerHTML = localStorage.getItem("carDescription");
+    }
+
+}
+
+////////////////////////////////////////////////////
+
+function changeDesc() {
+    let changeDescButton = document.getElementById("change-desc");
+    let saveDescButton = document.getElementById("save-desc");
+    let carDesc = document.getElementById("car-desc");
+    saveDescButton.style.display = "inline";
+    changeDescButton.style.display = "none";
+    carDesc.contentEditable = "true";
+    carDesc.style.border = "1px solid white";
+}
+
+function saveDesc() {
+    let changeDescButton = document.getElementById("change-desc");
+    let saveDescButton = document.getElementById("save-desc");
+    let carDesc = document.getElementById("car-desc");
+    if (carDesc.innerHTML == "" || (carDesc.innerHTML.length) < 6) {
+        carDesc.innerHTML = "Description can't be empty and it must be at least 6 characters long!";
+        return error;
+    } else {
+        saveDescButton.style.display = "none";
+        changeDescButton.style.display = "inline";
+        localStorage.setItem("carDescription", carDesc.innerHTML);
+        console.log(localStorage.getItem("carDescription"));
+        carDesc.contentEditable = "false";
+        carDesc.style.border = "none";
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+////////////////////////////////////////////
+
+window.onload = function() {
+    let site = document.getElementById("first-create");
+    site.style.opacity = "100%";
+    let mainText = document.getElementById("title");
+    if (localStorage.getItem("firstCar") != null) {
+        mainText.innerHTML = "Edit your car profile";
+        let carManu = document.getElementById("car-manufacturer");
+        let carModel = document.getElementById("car-model");
+        let carYear = document.getElementById("car-year");
+        let carEngine = document.getElementById("car-engine");
+        let carFuel = document.getElementById("car-fuel");
+        let carTransmission = document.getElementById("car-transm");
+        let car = JSON.parse(localStorage.getItem("firstCar"));
+        let addCarButton = document.getElementById("AddEdit");
+        let carOwner = document.getElementById("car-owner");
+        addCarButton.innerHTML = "Save";
+        carManu.value = car.manufacturer;
+        carModel.value = car.model;
+        carYear.value = car.year;
+        carEngine.value = car.engine;
+        carFuel.value = car.fuel;
+        carTransmission.value = car.transmission;
+        carOwner.value = car.owner;
+    } else {
+        mainText.innerHTML = "Let's create your first car profile";
+    }
+}
+
+class Car {
+    constructor(manufacturer, model, year, engine, fuel, transmission, owner) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+        this.engine = engine;
+        this.fuel = fuel;
+        this.transmission = transmission;
+        this.owner = owner;
+    }
+}
+
+function editCar() {
+    let carManu = document.getElementById("car-manufacturer").value;
+    let carModel = document.getElementById("car-model").value;
+    let carYear = document.getElementById("car-year").value;
+    let carEngine = document.getElementById("car-engine").value;
+    let carFuel = document.getElementById("car-fuel").value;
+    let carTransmission = document.getElementById("car-transm").value;
+    let carOwner = document.getElementById("car-owner").value;
+    if (carManu == "" || carModel == "" || carYear == "" || carEngine == "" || carFuel == "" || carTransmission == "" || carOwner == "") {
+        let error = document.getElementById("warning");
+        error.style.opacity = "100%";
+        return;
+    } else {
+        let firstCar = new Car(carManu, carModel, carYear, carEngine, carFuel, carTransmission, carOwner);
+        localStorage.setItem("firstCar", JSON.stringify(firstCar));
+        console.log(localStorage.getItem("firstCar"));
+        window.location.href = "./sites/car.html";
+    }
+}
+
+function debugReset() {
+    localStorage.clear();
+    console.log("Local storage cleared");
+}
