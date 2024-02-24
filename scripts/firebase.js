9ac3da5efbf8d741d10f2d9e9aabb121074f25ee
natsitarz <natsitarz@icloud.com>
2024-02-24 01:32:40 +0100

    Add Firebase dependencies and scripts

---
+++ scripts/firebase.js
@@ -0,0 +1,22 @@
+////////////////////////////////////////////
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "../node_modules/firebase/analytics";
+import { initializeApp } from "../node_modules/firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBFeREYEgrVj2cCzj71sP8pIAN3PXL-gQ0",
+    authDomain: "idmoto.firebaseapp.com",
+    projectId: "idmoto",
+    storageBucket: "idmoto.appspot.com",
+    messagingSenderId: "462593287282",
+    appId: "1:462593287282:web:1deb427333f34e50c34eeb",
+    measurementId: "G-8KBMZ08B5J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,3 @@
-////////////////////////////////////////////
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBFeREYEgrVj2cCzj71sP8pIAN3PXL-gQ0",
-    authDomain: "idmoto.firebaseapp.com",
-    projectId: "idmoto",
-    storageBucket: "idmoto.appspot.com",
-    messagingSenderId: "462593287282",
-    appId: "1:462593287282:web:1deb427333f34e50c34eeb",
-    measurementId: "G-8KBMZ08B5J"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 window.onload = function() {
     let site = document.getElementById("first-create");
     site.style.opacity = "100%";
