c0b57cf37ea680cdca0432dd511517ab6a3ab976
natsitarz <natsitarz@icloud.com>
2024-02-24 02:32:46 +0100

    Add new files and update dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "../node_modules/firebase/analytics";
-import { initializeApp } from "../node_modules/firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,7 +9,7 @@ import { initializeApp } from "../node_modules/firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBFeREYEgrVj2cCzj71sP8pIAN3PXL-gQ0",
-    authDomain: "idmoto.firebaseapp.com",
+    authDomain: "natsitarz.github.io",
     projectId: "idmoto",
     storageBucket: "idmoto.appspot.com",
     messagingSenderId: "462593287282",
